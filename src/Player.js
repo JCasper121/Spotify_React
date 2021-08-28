680bba530b76bc7afff010a7acc80ba4739f050c
JCasperGIT <jacqobin@gmail.com>
2021-08-28 00:59:02 -0600

    Init

---
+++ src/Player.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
+
+
+export default function Player({accessToken, trackUri}) {
+
+    const [play, setPlay] = useState('');
+
+    useEffect(() => setPlay(true), [trackUri])
+
+    if(!accessToken) return null;
+    return (
+        <div>
+            <SpotifyPlayer
+                token={accessToken}
+                showSaveIcon
+                uris={trackUri ? [trackUri] : []}
+                callback = {state => {
+                    if(!state.isPlaying) setPlay(false)
+                }}
+                play={play}
+                />
+    
+        </div>
+        )
+}
